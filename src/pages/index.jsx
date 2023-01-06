5a660ab67af877c636521a37686b5f546a935ada
Justin Esders <justin@hashsalt.net>
2023-01-05 19:00:38 -0500

    Replace images

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -224,6 +224,7 @@ function Resume() {
 
 function Photos() {
   let rotations = ['rotate-2', '-rotate-2', 'rotate-2', 'rotate-2', '-rotate-2']
+  let colors = ['bg-gradient-to-r from-indigo-500 to-indigo-800', 'bg-gradient-to-r from-cyan-500 to-blue-500', 'bg-gradient-to-b from-indigo-500 via-purple-500 to-pink-500', 'bg-gradient-to-l from-cyan-500 to-blue-500', 'bg-gradient-to-l from-indigo-500 to-indigo-800']
 
   return (
     <div className="mt-16 sm:mt-20">
@@ -232,16 +233,17 @@ function Photos() {
           <div
             key={image.src}
             className={clsx(
-              'relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 dark:bg-zinc-800 sm:w-72 sm:rounded-2xl',
-              rotations[imageIndex % rotations.length]
+              'relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 sm:w-72 sm:rounded-2xl',
+              rotations[imageIndex % rotations.length],
+              colors[imageIndex % colors.length]
             )}
           >
-            <Image
+            {/* <Image
               src={image}
               alt=""
               sizes="(min-width: 640px) 18rem, 11rem"
               className="absolute inset-0 h-full w-full object-cover"
-            />
+            /> */}
           </div>
         ))}
       </div>
