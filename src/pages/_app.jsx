588f8f0da15a03c674be2a29f191a22afc383d36
Justin Esders <justin@pixelparasol.com>
2024-04-01 17:20:56 -0400

    staging: Adding vercel speed insights

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -3,6 +3,7 @@ import { useEffect, useRef } from 'react'
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 import '@/styles/tailwind.css'
 import 'focus-visible'
@@ -32,6 +33,7 @@ export default function App({ Component, pageProps, router }) {
         <main>
           <Component previousPathname={previousPathname} {...pageProps} />
           <Analytics />
+          <SpeedInsights />
         </main>
         <Footer />
       </div>
